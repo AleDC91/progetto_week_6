5fbb3f18c25c48cdeccf4557a6f0a478d4dfac88
ale <ale91.dallas@gmail.com>
2023-12-12 17:33:04 +0100

    esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,25 +15,34 @@ class Product {
 
 let admin = true;
 
-// let p1 = new Product("rinvio", "rinvio 35 cm", "CT", "https:/vdfvadfv.sdfdfhvsf/dfsdfv", 17.90);
-// console.log(p1);
 
 document.addEventListener("DOMContentLoaded", () => {
-  // createProduct(URI, p1);
-  // getAllProducts(URI);
-  // getProduct(URI, "6578306fc058350018522e87");
-  // deleteProduct(URI, "6578306fc058350018522e87");
-  // getAllProducts(URI);
-
   if (admin && location.href.includes("index.html")) {
-
     let ul = document.querySelector("ul");
     let li = document.createElement("li");
     li.classList.add("nav-item");
-    li.innerHTML = `<a class="nav-link bg-primary rounded-2" href="admin.html">Admin</a>`;
+    li.innerHTML = `<a class="nav-link bg-primary rounded-2 text-white" href="admin.html">Admin</a>`;
     ul.appendChild(li);
-
     renderProducts(URI);
+    let container = document.querySelector(".products");
+
+    container.addEventListener("click", (e) => {
+// delete button
+      if(e.target.classList.contains("delete-btn")){
+        console.log(e.target.dataset.id)
+        deleteProduct(URI, e.target.dataset.id);
+      }
+// edit button
+      else if(e.target.classList.contains("edit-btn")){
+        console.log(e.target.dataset.id);
+        window.location.href = `edit.html?id=${e.target.dataset.id}`
+      }
+    })
+
+  }
+    else if(!admin && location.href.includes("index.html")){
+    renderProducts(URI);
+    
 
   } else if (admin && location.href.includes("admin.html")) {
 
@@ -43,26 +52,106 @@ document.addEventListener("DOMContentLoaded", () => {
     li.innerHTML = `<a class="nav-link bg-success rounded-2" href="index.html">Shop</a>`;
     ul.appendChild(li);
 
-    let createButton = document.querySelector('form button');
+    let createButton = document.querySelector("form button");
     // console.log(createButton);
 
     createButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        let productName = document.forms[0][1].value;
-        let productDescription = document.forms[0][2].value;
-        let brand = document.forms[0][3].value;
-        let price = document.forms[0][4].value;
-        let imageUrl = document.forms[0][5].value;
+      e.preventDefault();
+      let productName = document.forms[0][1].value;
+      let productDescription = document.forms[0][2].value;
+      let brand = document.forms[0][3].value;
+      let price = Number(document.forms[0][4].value);
+      let imageUrl = document.forms[0][5].value;
 
-        if ((!isNaN(+price)) && productName.length > 2 && productDescription.length > 2 && imageUrl.startsWith("http")){
-          let product = new Product(productName, productDescription, brand, imageUrl, price);
-          Product.total++;
-          createProduct(URI, product);
-                 }
-    })
+      if (
+        !isNaN(price) &&
+        productName.length > 2 &&
+        productDescription.length > 2 &&
+        imageUrl.startsWith("http")
+      ) {
+        let product = new Product(
+          productName,
+          productDescription,
+          brand,
+          imageUrl,
+          price
+        );
+        createProduct(URI, product);
+        document.forms[0][1].value = "";
+        document.forms[0][2].value = "";
+        document.forms[0][3].value = "";
+        document.forms[0][4].value = "";
+        document.forms[0][5].value = "";
+      }
+    });
+
+
+
+  }
+
+  else if(admin && location.href.includes("edit.html")){
+
+
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
+    li.classList.add("nav-item");
+    li.innerHTML = `<a class="nav-link bg-success rounded-2" href="index.html">Shop</a>`;
+    ul.appendChild(li);
+    
+    let createButton = document.querySelector("form button");
+    // console.log(createButton);
+    createButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      let productName = document.forms[0][1].value;
+      let productDescription = document.forms[0][2].value;
+      let brand = document.forms[0][3].value;
+      let price = Number(document.forms[0][4].value);
+      let imageUrl = document.forms[0][5].value;
+
+      if (
+        !isNaN(price) &&
+        productName.length > 2 &&
+        productDescription.length > 2 &&
+        imageUrl.startsWith("http")
+      ) {
+        let product = new Product(
+          productName,
+          productDescription,
+          brand,
+          imageUrl,
+          price
+        );
+        let searchParams = new URLSearchParams(window.location.search);
+        let productId = searchParams.get("id");
+        editProduct(URI, productId, product);
+        document.forms[0][1].value = "";
+        document.forms[0][2].value = "";
+        document.forms[0][3].value = "";
+        document.forms[0][4].value = "";
+        document.forms[0][5].value = "";
+      }
+    });
+
+
+  } else if(location.href.includes("product.html")){
+    let searchParams = new URLSearchParams(window.location.search);
+    let productId = searchParams.get("id");
+    getProduct(URI, productId);
 
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 function getAllProducts(uri) {
   fetch(uri, {
@@ -85,8 +174,15 @@ function createProduct(uri, data) {
       Authorization: `Bearer ${STRIVE_SCHOOL_API_KEY}`,
       "Content-Type": "application/json",
     },
-  }).then(window.location.href = "index.html")
-    .catch((err) => console.log(err));
+  })
+    .then((response) => {
+      if (response.ok) {
+        msg("success", "Prodotto aggiunto con successo!");
+      } else {
+        msg("error", "Errore! Prodotto non aggiunto" + response.status);
+      }
+    })
+    .catch((err) => msg("error", err));
 }
 
 function getProduct(uri, id) {
@@ -97,7 +193,7 @@ function getProduct(uri, id) {
     },
   })
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => showProduct(data));
 }
 
 function deleteProduct(uri, id) {
@@ -106,11 +202,20 @@ function deleteProduct(uri, id) {
     headers: {
       Authorization: `Bearer ${STRIVE_SCHOOL_API_KEY}`,
     },
-  }).catch((err) => console.log(err));
+  }).then((response) => {
+    if (response.ok) {
+      msg("success", "Prodotto eliminato con successo!");
+      renderProducts(uri);
+    } else {
+      msg("error", "Errore! Prodotto non cancellato" + response.status);
+    }
+  })
+  .catch((err) => msg("error", err));
 }
 
 function renderProducts(uri) {
   let productsContainer = document.querySelector(".products");
+  productsContainer.innerHTML = "";
   fetch(uri, {
     method: "GET",
     headers: {
@@ -128,16 +233,106 @@ function renderProducts(uri) {
           "col-md-4",
           "col-lg-3",
           "col-xxl-2",
-          "m-2"
+          "border-0",
+          "p-0",
+          "m-2",
+          "shadow"
         );
         card.innerHTML = `
                 <img src=${product.imageUrl} class="card-img-top" alt="${product.name}">
                 <div class="card-body">
                     <h5 class="card-title">${product.name}</h5>
                     <p class="card-text">${product.description}</p>
-                    <a href="" class="btn btn-primary">Vedi prodotto</a>
+                    <h5>${product.price.toFixed(2)}€</h5>
+                    <a href="product.html?id=${product._id}" class="btn btn-primary w-100 mb-2" data-id=${product._id}">Vedi prodotto</a>
+                    <div class="admin-buttons d-flex justify-content-between" data-id=${product._id}>
+                    <button class="btn btn-danger delete-btn" data-id=${product._id}>elimina</button>
+                    <button class="btn btn-warning edit-btn" data-id=${product._id}>modifica</button>
+                    </div>
                 </div>`;
         productsContainer.appendChild(card);
       });
+      hideButtons();
     });
+}
+
+function msg(type, message) {
+  let msg = document.querySelector(".msg");
+  msg.style.position = "fixed";
+  msg.style.zIndex = 10;
+  msg.style.width = "100%";
+  msg.style.margin = "0 auto"
+  msg.style.top = "50px"
+  if (type === "error") {
+    msg.innerHTML = `<div class="alert alert-danger w-75 text-center mx-auto mt-2" role="alert">
+                          Errore! ${message}
+                    </div>`;
+
+    setTimeout(() => {
+      msg.innerHTML = "";
+    }, 3000);
+
+  } else if (type === "success") {
+
+    msg.innerHTML = `<div class="alert alert-success w-75 text-center mx-auto mt-2" role="alert">
+                          ${message}
+                    </div>`;
+    setTimeout(() => {
+      msg.innerHTML = "";
+    }, 3000);
+  }
+}
+function hideButtons(){
+  let cards = document.querySelectorAll(".card");
+  console.log(cards)
+
+  if(!admin){
+    cards.forEach(card => {
+      let buttonsToHide = card.querySelector(".admin-buttons");
+      // console.log(cards)
+      buttonsToHide.innerHTML = "";
+    })
+  }
+}
+
+function editProduct(uri, id, data){
+  fetch(`${uri}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: `Bearer ${STRIVE_SCHOOL_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        msg("success", "Prodotto modificato con successo!");
+      } else {
+        msg("error", "Errore! Prodotto non modificato" + response.status);
+      }
+    })
+    .catch((err) => msg("error", err));
+}
+
+
+function showProduct(product){
+  let productContainer = document.querySelector(".product");
+  let card = document.createElement("div");
+  card.classList.add(
+    "card",
+    "col-12",
+    "col-md-4",
+    "col-lg-3",
+    "col-xxl-2",
+    "m-2"
+  );
+  card.innerHTML = `
+          <img src=${product.imageUrl} class="card-img-top" alt="${product.name}">
+          <div class="card-body">
+              <h5 class="card-title">${product.name}</h5>
+              <p class="card-text">${product.description}</p>
+              <h5>${product.price}€</h5>
+              </div>
+          </div>`;
+  productContainer.appendChild(card);
 }
